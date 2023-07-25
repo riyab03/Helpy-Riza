d7957f827d457013ae40448412402db5802a306b
riyab03 <100680089+riyab03@users.noreply.github.com>
2023-07-25 15:52:11 +0530

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import DataRequired
+
+
+class chatbotform(FlaskForm):
+    user_input=StringField(validators=[DataRequired()])
+    send=SubmitField('Send')
